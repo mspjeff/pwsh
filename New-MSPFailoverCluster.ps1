06a77a289c4ccbdd2058ef146f43e035dd08e19b
Jeff <166336541+mspjeff@users.noreply.github.com>
2024-05-15 15:21:04 -0700

    fixed some things

--- New-MSPFailoverCluster.ps1
+++ New-MSPFailoverCluster.ps1
@@ -1,57 +1,78 @@
+<#
+    .SYNOPSIS
+        Creates a new Failover Cluster with shared iSCSI storage
+    .DESCRIPTION
+        Prepares a Hyper-V server for failover clustering and creates a
+        failover cluster.
+    .EXAMPLE
+        PS C:\> New-MSPFailoverCluster.ps1
+    .EXAMPLE
+        PS C:\> irm https://raw.githubusercontent.com/mspjeff/pwsh/main/New-MSPFailoverCluster.ps1 | iex
+    .LINK
+        https://github.com/mspjeff/pwsh
+#>
+[CmdletBinding()]
 param (
-	[Parameter(Required=$true)]
-	[string]
-	$ClusterName,
+    [Parameter(Mandatory=$true, HelpMessage="Name of new failover cluster")]
+    [string]
+    $ClusterName,
 
-	[Parameter(Required=$true)]
-	[string]
-	$NASAddress
+    [Parameter(Mandatory=$true, HelpMessage="IPv4 address of iSCSI target")]
+    [string]
+    $NASAddress
 )
+
 if (-not (Get-WindowsFeature Failover-Clustering).State -eq 'Installed')
 {
-	Install-WindowsFeature -Name Failover-Clustering -IncludeManagementTools
-	Write-Warning "Restarting after installing clustering feature"
-	Restart-Computer -Force
+    Install-WindowsFeature -Name Failover-Clustering -IncludeManagementTools
+    Write-Warning "Restarting after installing clustering feature"
+    Restart-Computer -Force
+}
+
+if (Get-Cluster -Name $ClusterName)
+{
+    Write-Warning "There is already a failover cluster named $ClusterName"
+    return
 }
 
 if (Get-Service -Name MSiSCSI)
 {
-	Write-Warning "MSiSCSI service is missing"
-	return
+    Write-Warning "MSiSCSI service is missing"
+    return
 }
 
 if ((Get-Service -Name MSiSCSI).StartType -ne 'Automatic')
 {
-	Set-Service -Name MSiSCSI -StartupType Automatic
+    Set-Service -Name MSiSCSI -StartupType Automatic
 }
 
 if ((Get-Service -Name MSiSCSI).Status -ne 'Running')
 {
-	Start-Service -Name MSiSCSI
+    Start-Service -Name MSiSCSI
 }
 
 if (-not (Test-NetConnection $NASAddress))
 {
-	Write-Warning "Unable to ping NAS address at $NASAddress"
-	return
+    Write-Warning "Unable to ping NAS address at $NASAddress"
+    return
 }
 
 if (-not (Get-IscsiTargetPortal -TargetPortalAddress $NASAddress))
 {
-	New-IscsiTargetPortal -TargetPortalAddress $NASAddress
+    New-IscsiTargetPortal -TargetPortalAddress $NASAddress
 }
 
 if ($target = Get-IscsiTargetPortal -TargetPortalAddress $NASAddress | Get-IscsiTarget | Where-Object {$_.IsConnected -eq $false})
 {
-	$target | Connect-IscsiTarget -IsPersistent $true
+    $target | Connect-IscsiTarget -IsPersistent $true
 }
 
 if ($disk = Get-Disk | Where-Object {$_.OperationalStatus -eq 'Offline' -and $_.PartitionStyle -eq 'Raw'})
 {
-	Initialize-Disk -Number $disk.Number -PartitionStyle GPT -PassThru `
-	| Set-Disk -IsOffline $false `
-	| New-Partition -UseMaximumSize -AssignDriveLetter:$false `
-	| Format-Volume -FileSystem NTFS -NewFileSystemLabel 'Clustered'
+    Initialize-Disk -Number $disk.Number -PartitionStyle GPT -PassThru `
+    | Set-Disk -IsOffline $false `
+    | New-Partition -UseMaximumSize -AssignDriveLetter:$false `
+    | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'Clustered'
 }
 
 New-Cluster -Name $ClusterName
--- Start-WindowsExporter.ps1
+++ Start-WindowsExporter.ps1
@@ -3,121 +3,121 @@
         Starts the Windows Exporter for surfacing metrics to Prometheus.
     .DESCRIPTION
         This script starts an instance of the Windows Exporter with the
-	appropriate machine-specific collectors enabled without having to
-	manually specify the enabled collector list.
+        appropriate machine-specific collectors enabled without having to
+        manually specify the enabled collector list.
     .EXAMPLE
         PS C:\> Start-WindowsExporter
     .EXAMPLE
-        PS C:\> iwr https://... | iex
+        PS C:\> irm https://raw.githubusercontent.com/mspjeff/pwsh/main/Start-WindowsExporter.ps1 | iex
     .LINK
         https://github.com/prometheus-community/windows_exporter
   #>
 
 [CmdletBinding()]
 param(
-	[Parameter(Mandatory=$false)]
-	[string][ValidateNotNullOrEmpty()]
-	$WebListenAddress = ":9182",
+    [Parameter(Mandatory=$false)]
+    [string][ValidateNotNullOrEmpty()]
+    $WebListenAddress = ":9182",
 
-	[Parameter(Mandatory=$false)]
-	[switch]
-	$NoStart = $false
+    [Parameter(Mandatory=$false)]
+    [string][ValidateNotNullOrEmpty()]
+    $ExecutablePath = "$env:ProgramData\Prometheus\windows_exporter-0.25.1-amd64.exe",
+
+    [Parameter(Mandatory=$false)]
+    [switch]
+    $NoStart = $false
 )
 
-function Get-WindowsExporterExecutable
+function Get-Executable
 {
-	$filename = "windows_exporter-0.25.1-amd64.exe"
-	$sourceUrl = "https://github.com/prometheus-community/windows_exporter/releases/download/v0.25.1/$filename"
-	$destinationPath = Join-Path $env:ProgramData "Prometheus"
-	$destinationPathAndFilename = Join-Path $destinationPath $filename
-	if (-not (Test-Path $destinationPathAndFilename))
-	{
-		if (-not (Test-Path $destinationPath))
-		{
-			Write-Information "Creating $destinationPath"
-			New-Item -ItemType Directory -Path $destinationPath
-		}
-		Write-Information "Downloading $filename from $sourceUrl"
-		Start-BitsTransfer -Source $sourceUrl -Destination $destinationPath
-	}
-	return $destinationPathAndFilename
+    $filename = "windows_exporter-0.25.1-amd64.exe"
+    $sourceUrl = "https://github.com/prometheus-community/windows_exporter/releases/download/v0.25.1/$filename"
+    $destinationPath = Split-Path -Path $ExecutablePath
+    if (-not (Test-Path $ExecutablePath))
+    {
+        if (-not (Test-Path $destinationPath))
+        {
+            Write-Information "Creating $destinationPath"
+            New-Item -ItemType Directory -Path $destinationPath | Out-Null
+        }
+        Write-Information "Downloading $filename from $sourceUrl"
+        Start-BitsTransfer -Source $sourceUrl -Destination $ExecutablePath
+    }
 }
 
-function Get-WindowsExporterCollectors
+function Get-Collectors
 {
-	$collectors = @(
-		'cpu',
-		'cs',
-		'logical_disk',
-		'net',
-		'os',
-		'service',
-		'system',
-		'textfile'
-	)
+    $collectors = @(
+        'cpu',
+        'cs',
+        'logical_disk',
+        'net',
+        'os',
+        'system'
+    )
 
-	$serviceToCollectorHash = @{
-		ntds = 'ad'
-		dfsr = 'dfsr'
-		dhcpserver = 'dhcp'
-		dns = 'dns'
-		vmms = 'hyperv'
-		w3svc = 'iis'
-		mssqlserver = 'mssql'
-		termservice = 'terminal_services'
-	}
+    $serviceToCollectorHash = @{
+        ntds = 'ad'
+        dfsr = 'dfsr'
+        dhcpserver = 'dhcp'
+        dns = 'dns'
+        vmms = 'hyperv'
+        w3svc = 'iis'
+        mssqlserver = 'mssql'
+        termservice = 'terminal_services'
+    }
 
-	foreach ($key in $serviceToCollectorHash.Keys)
-	{
-		if ((Get-Service $key -ErrorAction SilentlyContinue).Status -eq 'Running')
-		{
-			$collectors += $serviceToCollectorHash[$key]
-		}
-	}
+    foreach ($key in $serviceToCollectorHash.Keys)
+    {
+        if ((Get-Service $key -ErrorAction SilentlyContinue).Status -eq 'Running')
+        {
+            $collectors += $serviceToCollectorHash[$key]
+        }
+    }
 
-	return $collectors -join ','
+    return $collectors -join ','
 }
 
-function Get-WindowsExporterServices
+function Get-Services
 {
-	$candidates = @{
-		"NTDS" = @(
-			"NTDS",    # Active Directory Domain Services
-			"DNS",     # DNS Server
-			"kdc",     # Kerberos Key Distribution Center
-			"Netlogon",# Net Logon
-			"IsmServ", # Intersite Messaging
-			"NtFrs",   # File Replication Service
-			"DFSR"     # Distributed File System Replication
-		)
-	}
+    $candidates = @{
+        "NTDS" = @(
+            "NTDS",    # Active Directory Domain Services
+            "DNS",     # DNS Server
+            "kdc",     # Kerberos Key Distribution Center
+            "Netlogon",# Net Logon
+            "IsmServ", # Intersite Messaging
+            "NtFrs",   # File Replication Service
+            "DFSR"     # Distributed File System Replication
+        )
+    }
 
-	$services = @()
+    $services = @()
 
-	foreach ($key in $candidates.Keys)
-	{
-		if ((Get-Service $key -ErrorAction SilentlyContinue).Status -eq 'Running')
-		{
-			$services += $candidates[$key]
-		}
-	}
+    foreach ($key in $candidates.Keys)
+    {
+        if ((Get-Service $key -ErrorAction SilentlyContinue).Status -eq 'Running')
+        {
+            $services += $candidates[$key]
+        }
+    }
 
-	return ($services | ForEach-Object { "Name='$_'" }) -join " OR "
+    return ($services | ForEach-Object { "Name='$_'" }) -join " OR "
 }
 
 $InformationPreference = 'Continue'
 
-$executable = Get-WindowsExporterExecutable
-Write-Information "Executable to run: $executable"
+Get-Executable
+Write-Information "Executable to run: '$ExecutablePath'"
 
-$collectors = Get-WindowsExporterCollectors
-Write-Information "Collectors to be enabled are: $collectors"
+$collectors = Get-Collectors
+Write-Information "Collectors to be enabled are: '$collectors'"
 
-$services = Get-WindowsExporterServices
-Write-Information "Services to be enabled are: $services"
+$services = Get-Services
+Write-Information "Services to be enabled are: '$services'"
 
 if (-not $NoStart)
 {
-	Write-Information "Starting $executable"
-	& $executable --collectors.enabled "$collectors"
+    Write-Information "Starting '$ExecutablePath'"
+    & $ExecutablePath --collectors.enabled "$collectors"
 }
